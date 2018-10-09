fad2547ea0abfa9a34849aa0e9a6a0936a7eaf5b
swilliam01 <sehrishwilliam@gamil.com>
2018-10-09 16:33:54 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,147 @@
+
+// Adding the sum after user input 10 numbers
+/******************************************************
+//package com.company;
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Random rand = new Random();
+//        int total = 0;
+//        int [] input = new int[10];
+//        int num;
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        int counter;
+//        for (counter = 0 ; counter < input.length ; counter++) {
+//            input [counter]= counter + 1;
+//            System.out.println("Enter a number : ");
+//            num = keyboard.nextInt();
+//            total = total + num;
+//        }
+//        System.out.println(total);
+//    }
+//}
+ *********************************************************/
+// Calculating the average of 10 numbers inputted by the user
+/********************************************************
+//package com.company;
+//
+//        import java.util.Random;
+//        import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Random rand = new Random();
+//        int total = 0;
+//        int average;
+//        int [] input = new int[10];
+//        int num;
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        int counter;
+//        for (counter = 0 ; counter < input.length ; counter++) {
+//            input [counter]= counter + 1;
+//            System.out.println("Enter a number : ");
+//            num = keyboard.nextInt();
+//            total = total + num;
+//        }
+//        average = total / input.length;
+//        System.out.println(average);
+//    }
+//}
+ ********************************************************/
+//Printing 3 names
+/*************************************
+//package com.company;
+//
+//        import java.util.Random;
+//        import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Random rand = new Random();
+//
+//        String[] names = new String[]{"Sherry", "Mike", "Barry"};
+//        //Scanner keyboard = new Scanner(System.in);
+//
+//        int counter;
+//        for (counter = 0; counter < 3; counter++) {
+//
+//            System.out.println(names[counter]);
+//
+//        }
+//
+//    }
+//}
+ **********************************************/
+// Getting index for a specific value
+/***************************************
+//package com.company;
+//
+//        import java.util.Random;
+//        import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Random rand = new Random();
+//        int[] value2 = {2,5,9,0,2,1,8,5,4};
+//
+//        //Scanner keyboard = new Scanner(System.in);
+//
+//        int counter;
+//        for (counter = 0; counter < value2.length; counter++) {
+//
+//            if (value2[counter] == 5)
+//                System.out.println(counter );
+//
+//        }
+//
+//    }
+//}
+
+ *****************************************/
+ // print number 1-10 backwards
+/*******************************
+package com.company;
+
+        import java.util.Random;
+        import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int[] value2 = {1,2,3,4,5,6,7,8,9,10};
+
+        //Scanner keyboard = new Scanner(System.in);
+
+        int counter;
+        for (counter = 9; counter >=0; counter--) {
+            //  value2[counter] =  counter + 1;
+            System.out.println(value2[counter]);
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
